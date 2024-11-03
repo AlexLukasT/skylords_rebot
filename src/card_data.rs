41a10a5d3e97aa541f07ce99ce423459dee2b237
AlexLukasT <alexander.temme@gmx.de>
2024-11-03 07:43:11 +0100

    WIP Working on spawn policies

--- src/card_data.rs
+++ src/card_data.rs
@@ -5,6 +5,7 @@ use std::collections::BTreeMap;
 use std::env;
 use std::fs;
 use std::path;
+use std::str::FromStr;
 
 use crate::game_info::PlayerInfo;
 
@@ -12,11 +13,10 @@ const CARD_INFO_FILE_PATH: &'static str = "data/cards.json";
 
 pub struct CardData {
     data: serde_json::Value,
-    power_cost_cache: BTreeMap<String, f32>,
-    orb_requirement_cache: BTreeMap<String, CardOrbRequirements>,
+    card_info_cache: BTreeMap<String, CardInfo>,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub struct CardOrbRequirements {
     total: i32,
     neutral: i32,
@@ -26,25 +26,155 @@ pub struct CardOrbRequirements {
     frost: i32,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, PartialOrd, Ord)]
+pub enum CardOffenseType {
+    S,
+    M,
+    L,
+    XL,
+    Special,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, PartialOrd, Ord)]
+pub enum CardDefenseType {
+    S,
+    M,
+    L,
+    XL,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct CardInfo {
+    id: i32,
+    power_cost: f32,
+    orb_requirements: CardOrbRequirements,
+    offense_type: CardOffenseType,
+    defense_type: CardDefenseType,
+}
+
+impl CardInfo {
+    pub fn new() -> CardInfo {
+        CardInfo {
+            id: 0,
+            power_cost: 0.,
+            orb_requirements: CardOrbRequirements {
+                total: 0,
+                neutral: 0,
+                fire: 0,
+                shadow: 0,
+                nature: 0,
+                frost: 0,
+            },
+            offense_type: CardOffenseType::S,
+            defense_type: CardDefenseType::S,
+        }
+    }
+}
+
+impl FromStr for CardOffenseType {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<CardOffenseType, Self::Err> {
+        match input {
+            "S" => Ok(CardOffenseType::S),
+            "M" => Ok(CardOffenseType::M),
+            "L" => Ok(CardOffenseType::L),
+            "XL" => Ok(CardOffenseType::XL),
+            "Special" => Ok(CardOffenseType::Special),
+            _ => Err(()),
+        }
+    }
+}
+
+impl FromStr for CardDefenseType {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<CardDefenseType, Self::Err> {
+        match input {
+            "S" => Ok(CardDefenseType::S),
+            "M" => Ok(CardDefenseType::M),
+            "L" => Ok(CardDefenseType::L),
+            "XL" => Ok(CardDefenseType::XL),
+            _ => Err(()),
+        }
+    }
+}
+
 impl CardData {
     pub fn new() -> Self {
         CardData {
             data: serde_json::Value::Null,
-            power_cost_cache: BTreeMap::new(),
-            orb_requirement_cache: BTreeMap::new(),
+            card_info_cache: BTreeMap::new(),
         }
     }
 
-    fn get_card(&self, card_template: &CardTemplate) -> Option<&serde_json::Value> {
-        let name = card_template.name().to_lowercase();
-        debug!("Searching for card name {name:?}");
+    pub fn get_card_info_from_id(&self, card_id: i32) -> CardInfo {
+        let card_option = self.get_card_from_id(card_id);
+        if let Some(card) = card_option {
+            self.get_card_info_from_card(card)
+        } else {
+            CardInfo::new()
+        }
+    }
+
+    pub fn get_card_info_card_template(&self, card_template: &CardTemplate) -> CardInfo {
+        let name = card_template.name().to_lowercase().to_string();
+        self.get_card_info_from_name(name)
+    }
+
+    fn get_card_info_from_name(&mut self, name: String) -> CardInfo {
+        if let Some(card_info) = self.card_info_cache.get(&name) {
+            return *card_info;
+        }
+
+        if let Some(card) = self.get_card_from_name(&name) {
+            let card_info = self.get_card_info_from_card(card);
+            self.card_info_cache.insert(name, card_info);
+            card_info
+        } else {
+            CardInfo::new()
+        }
+    }
+
+    fn get_card_info_from_card(&self, card: &serde_json::Value) -> CardInfo {
+        CardInfo {
+            id: self.get_card_id(card),
+            power_cost: self.get_card_power_cost(card),
+            orb_requirements: self.get_card_orbs(card),
+            offense_type: self.get_card_offense_type(card),
+            defense_type: self.get_card_defense_type(card),
+        }
+    }
+
+    pub fn card_id_without_upgrade(id: i32) -> i32 {
+        if id >= (Upgrade::U3 as i32) {
+            return id - (Upgrade::U3 as i32);
+        }
+        id
+    }
+
+    fn get_card_from_id(&self, card_id: i32) -> Option<&serde_json::Value> {
         for card in self.data["data"].as_array().unwrap() {
-            if card["cardSlug"].as_str().unwrap().replace("-", "") == name {
+            let ids = card["officialCardIds"].as_array().unwrap();
+            for id in ids {
+                if id.as_i64().unwrap() as i32 == CardData::card_id_without_upgrade(card_id) {
+                    return Some(card);
+                }
+            }
+        }
+
+        error!("Unable to find card with ID {card_id:?}");
+        None
+    }
+
+    fn get_card_from_name(&self, name: &String) -> Option<&serde_json::Value> {
+        for card in self.data["data"].as_array().unwrap() {
+            if card["cardSlug"].as_str().unwrap().replace("-", "") == *name {
                 return Some(card);
             }
         }
 
-        warn!("Unable to find card {card_template:?} in card data");
+        error!("Unable to find card {name:?} in card data");
         None
     }
 
@@ -61,54 +191,55 @@ impl CardData {
         debug!("Finished loading card data");
     }
 
-    pub fn get_card_power_cost(&mut self, card_template: &CardTemplate) -> f32 {
-        let name = card_template.name().to_lowercase().to_string();
+    fn get_card_id(&self, card: &serde_json::Value) -> i32 {
+        let ids = card["officialCardIds"].as_array().unwrap();
 
-        if let Some(power_cost) = self.power_cost_cache.get(&name) {
-            return *power_cost;
+        if ids.len() == 0 {
+            error!("Unable to find CardId for {card:?}");
+            0
+        } else if ids.len() > 1 {
+            warn!("Got more than one CardId for {card:?}");
+            0
+        } else {
+            ids.get(0).unwrap().as_i64().unwrap() as i32
         }
-
-        if let Some(card) = self.get_card(card_template) {
-            let power_cost = card["powerCost"].as_array().unwrap()[3].as_f64().unwrap() as f32;
-
-            self.power_cost_cache.insert(name, power_cost);
-
-            return power_cost;
-        }
-
-        0.
     }
 
-    pub fn get_card_orbs(&mut self, card_template: &CardTemplate) -> CardOrbRequirements {
-        let name = card_template.name().to_lowercase().to_string();
+    fn get_card_power_cost(&self, card: &serde_json::Value) -> f32 {
+        card["powerCost"].as_array().unwrap()[3].as_f64().unwrap() as f32
+    }
 
-        if let Some(orb_requirements) = self.orb_requirement_cache.get(&name) {
-            return *orb_requirements;
-        }
-
-        if let Some(card) = self.get_card(card_template) {
-            let orb_requirements = CardOrbRequirements {
-                total: card["orbsTotal"].as_f64().unwrap() as i32,
-                neutral: card["orbsNeutral"].as_i64().unwrap() as i32,
-                fire: card["orbsFire"].as_i64().unwrap() as i32,
-                shadow: card["orbsShadow"].as_i64().unwrap() as i32,
-                nature: card["orbsNature"].as_i64().unwrap() as i32,
-                frost: card["orbsFrost"].as_i64().unwrap() as i32,
-            };
-
-            self.orb_requirement_cache.insert(name, orb_requirements);
-
-            return orb_requirements;
-        }
-
+    fn get_card_orbs(&self, card: &serde_json::Value) -> CardOrbRequirements {
         CardOrbRequirements {
-            total: 0,
-            neutral: 0,
-            fire: 0,
-            shadow: 0,
-            nature: 0,
-            frost: 0,
+            total: card["orbsTotal"].as_i64().unwrap() as i32,
+            neutral: card["orbsNeutral"].as_i64().unwrap() as i32,
+            fire: card["orbsFire"].as_i64().unwrap() as i32,
+            shadow: card["orbsShadow"].as_i64().unwrap() as i32,
+            nature: card["orbsNature"].as_i64().unwrap() as i32,
+            frost: card["orbsFrost"].as_i64().unwrap() as i32,
         }
+    }
+
+    fn get_card_offense_type(&self, card: &serde_json::Value) -> CardOffenseType {
+        let offense_types = self.data.get("enums").unwrap().get("offenseType").unwrap();
+        let index = card["offenseType"].as_i64().unwrap();
+        let offense_type = offense_types
+            .get(index.to_string())
+            .unwrap()
+            .as_str()
+            .unwrap();
+        CardOffenseType::from_str(offense_type).unwrap()
+    }
+
+    fn get_card_defense_type(&self, card: &serde_json::Value) -> CardDefenseType {
+        let defense_types = self.data.get("enums").unwrap().get("defenseType").unwrap();
+        let index = card["defenseType"].as_i64().unwrap();
+        let defense_type = defense_types
+            .get(index.to_string())
+            .unwrap()
+            .as_str()
+            .unwrap();
+        CardDefenseType::from_str(defense_type).unwrap()
     }
 
     pub fn player_fullfills_orb_requirements(
@@ -116,7 +247,7 @@ impl CardData {
         card_template: &CardTemplate,
         player_info: &PlayerInfo,
     ) -> bool {
-        let orb_requirements = self.get_card_orbs(card_template);
+        let orb_requirements = self.get_card_info(card_template).orb_requirements;
 
         // fire, shadow, nature, frost
         let mut num_colors: Vec<i32> = vec![0; 4];
--- src/controller/spawn_controller.rs
+++ src/controller/spawn_controller.rs
@@ -2,10 +2,12 @@ use api::sr_libs::utils::card_templates::CardTemplate;
 use api::sr_libs::utils::card_templates::CardTemplate::*;
 use api::*;
 use log::*;
+use sr_libs::cff::card::Card;
 
 use crate::command_scheduler::CommandScheduler;
 use crate::controller::squad_controller::SquadController;
 use crate::game_info::GameInfo;
+use crate::card_data::*;
 
 // minimum difference in bound power to spawn a new squad when matching opponent's army size
 const MIN_POWER_DIFF_SPAWN: f32 = 50.;
@@ -14,6 +16,9 @@ const MIN_POWER_DIFF_SPAWN: f32 = 50.;
 pub struct SpawnController {
     state: SpawnControllerState,
     spawn_pos: Position2D,
+    tier1_offense_spawn_policy: Option<Vec<CardTemplate>>,
+    tier2_offense_spawn_policy: Option<Vec<CardTemplate>>,
+    in_offense: bool,
 }
 
 #[derive(Debug, Default, PartialEq)]
@@ -30,6 +35,9 @@ impl SpawnController {
         SpawnController {
             state: SpawnControllerState::Waiting,
             spawn_pos: Position2D { x: 0., y: 0. },
+            tier1_offense_spawn_policy: None,
+            tier2_offense_spawn_policy: None,
+            in_offense: true,
         }
     }
 
@@ -40,13 +48,10 @@ impl SpawnController {
     ) -> Vec<SquadController> {
         // TODO: handle T2 + T3
 
-        let next_card: CardTemplate;
-        if game_info.bot.squads.is_empty() {
-            // no squads -> always start with a Dreadcharger
-            next_card = Dreadcharger;
-        } else {
-            next_card = Forsaken;
-        }
+        self.set_spawn_policy(game_info);
+
+        let next_card = self.get_next_card();
+
         let num_squads = game_info.bot.squads.keys().len();
 
         match self.state {
@@ -130,5 +135,118 @@ impl SpawnController {
     fn enter_state(&mut self, new_state: SpawnControllerState) {
         info!("SpawnController entered state {:?}", new_state);
         self.state = new_state;
+    }
+
+    fn set_spawn_policy(&mut self, game_info: &GameInfo) {
+        if game_info.opponent.token_slots.len() == 1 && self.tier1_offense_spawn_policy.is_none() {
+            let token_slots: Vec<&TokenSlot> = game_info.opponent.token_slots.values().collect();
+            let orb_color = token_slots[0].color;
+            info!("Setting T1 spawn policy to {orb_color:?}");
+            self.tier1_offense_spawn_policy =
+                Some(SpawnController::get_tier1_offense_spawn_policy(orb_color));
+        }
+
+        if game_info.opponent.token_slots.len() == 2 && self.tier2_offense_spawn_policy.is_none() {
+            let token_slots: Vec<&TokenSlot> = game_info.opponent.token_slots.values().collect();
+            let orb_colors = (token_slots[0].color, token_slots[1].color);
+            info!("Setting T2 spawn policy to {orb_colors:?}");
+            self.tier1_offense_spawn_policy =
+                Some(SpawnController::get_tier1_offense_spawn_policy(orb_colors));
+        }
+    }
+
+    pub fn set_in_offense(&mut self, in_offense: bool) {
+        self.in_offense = in_offense;
+    }
+
+    fn get_next_card(&self, game_info: &GameInfo) -> CardTemplate {
+        let card_policy: Vec<CardTemplate>;
+        if game_info.bot.token_slots.len() == 2 {
+            // try T2 first
+            if self.in_offense {
+                // when in offense choose cards based on fixed policy
+                card_policy = self.tier2_offense_spawn_policy;
+            } else {
+                // when in defense choose cards based on enemy squads
+                card_policy = vec![];
+            }
+        } else {
+            // T1
+            if self.in_offense {
+                card_policy = self.tier1_offense_spawn_policy;
+            } else {
+                card_policy = vec![];
+            }
+        }
+    }
+
+    fn get_t1_defense_spawn_policy(&self, game_info: &GameInfo) {
+        // choose defending units based on attacking ones
+        let opponent_squads: Vec<&Squad> = game_info.opponent.squads.values().collect();
+        let squad_offense_types: Vec<CardOffenseType> = opponent_squads.iter().map(
+            |&s| CardId::
+        )
+    }
+
+    fn get_tier1_offense_spawn_policy(opponent_color: OrbColor) -> Vec<CardTemplate> {
+        match opponent_color {
+            OrbColor::Fire | OrbColor::Shadow => vec![Dreadcharger, Forsaken],
+            OrbColor::Frost | OrbColor::Nature => vec![Dreadcharger, Forsaken, NoxTrooper],
+            _ => vec![],
+        }
+    }
+
+    fn get_tier2_offense_spawn_policy(opponent_colors: (OrbColor, OrbColor)) -> Vec<CardTemplate> {
+        match opponent_colors {
+            // Pure Fire
+            (OrbColor::Fire, OrbColor::Fire) => vec![Nightcrawler, AmiiPhantom],
+
+            // Pure Shadow
+            (OrbColor::Shadow, OrbColor::Shadow) => {
+                vec![Nightcrawler, DarkelfAssassins, AmiiPhantom]
+            }
+
+            // Pure Nature
+            (OrbColor::Nature, OrbColor::Nature) => {
+                vec![Burrower, DarkelfAssassins]
+            }
+
+            // Pure Frost
+            (OrbColor::Frost, OrbColor::Frost) => {
+                vec![AmiiPaladins, DarkelfAssassins]
+            }
+
+            // Fire Nature
+            (OrbColor::Fire, OrbColor::Nature) | (OrbColor::Nature, OrbColor::Fire) => {
+                vec![Nightcrawler, AmiiPhantom]
+            }
+
+            // Fire Shadow
+            (OrbColor::Fire, OrbColor::Shadow) | (OrbColor::Shadow, OrbColor::Fire) => {
+                vec![Nightcrawler, AmiiPhantom]
+            }
+
+            // Fire Frost
+            (OrbColor::Fire, OrbColor::Frost) | (OrbColor::Frost, OrbColor::Fire) => {
+                vec![Nightcrawler, DarkelfAssassins]
+            }
+
+            // Shadow Nature
+            (OrbColor::Shadow, OrbColor::Nature) | (OrbColor::Nature, OrbColor::Shadow) => {
+                vec![Nightcrawler, AmiiPhantom]
+            }
+
+            // Shadow Frost
+            (OrbColor::Shadow, OrbColor::Frost) | (OrbColor::Frost, OrbColor::Shadow) => {
+                vec![Nightcrawler, DarkelfAssassins, AmiiPhantom]
+            }
+
+            // Nature Frost
+            (OrbColor::Nature, OrbColor::Frost) | (OrbColor::Frost, OrbColor::Nature) => {
+                vec![Nightcrawler, AmiiPhantom]
+            }
+
+            _ => vec![],
+        }
     }
 }
